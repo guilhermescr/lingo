350ff52011c251f176606e3e6312cf316420854c
guilhermescr <guilhermechaves2005@hotmail.com>
2024-12-29 18:55:59 -0300

    feat: add header, wrapper and sidebar for learn page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -8,11 +13,44 @@ export const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex flex-col bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2",
+        "flex flex-col h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2",
         className,
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            className="scale-90"
+            src="/happy-brain-mascot.png"
+            alt="Happy Brain Lingo Mascot"
+            width={65}
+            height={65}
+          />
+          <h1 className="text-2xl font-extrabold text-pink-400 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
